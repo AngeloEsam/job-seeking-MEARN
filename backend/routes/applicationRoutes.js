618e226dbb38baee04371e47f4ba84cc637e0689
Angelo Esam <angloesam61@gmail.com>
2024-07-24 16:36:31 +0300

    finish backend

--- backend/routes/applicationRoutes.js
+++ backend/routes/applicationRoutes.js
@@ -1,4 +1,14 @@
-import express from 'express'
-
-const router = express.Router()
-export default router
+import express from "express";
+import { isAuthenticated } from "./../middlewares/auth.js";
+import {
+  employerGetAllApplications,
+  jobseekerGetAllApplications,
+  postApplication,
+  jobseekerDeleteApplication
+} from "../controllers/applicationController.js";
+const router = express.Router();
+router.post("/post", isAuthenticated, postApplication);
+router.get("/employer/getall", isAuthenticated, employerGetAllApplications);
+router.get("/jobseeker/getall", isAuthenticated, jobseekerGetAllApplications);
+router.delete("/delete/:id", isAuthenticated, jobseekerDeleteApplication);
+export default router;
